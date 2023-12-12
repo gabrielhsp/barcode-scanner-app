b12077bb782b90be582517eaa6962a3a80095051
Gabriel Henrique <gabhenrique0@gmail.com>
2023-12-11 22:26:20 -0300

    Feature: replace Rectangle element with ScannerView

--- BarcodeScanner/BarcodeScannerView.swift
+++ BarcodeScanner/BarcodeScannerView.swift
@@ -11,7 +11,7 @@ struct BarcodeScannerView: View {
     var body: some View {
         NavigationView {
             VStack {
-                Rectangle()
+                ScannerView()
                     .frame(maxWidth: .infinity, maxHeight: 300)
                 
                 Spacer()
