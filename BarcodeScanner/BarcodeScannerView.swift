c74a8a25a29d793f4dacb1785023addea5aaf10b
Gabriel Henrique <gabhenrique0@gmail.com>
2023-12-11 22:37:14 -0300

    Feature: add a @State var on barcode scanner view and customize UI using the scanned code passed as reference

--- BarcodeScanner/BarcodeScannerView.swift
+++ BarcodeScanner/BarcodeScannerView.swift
@@ -8,10 +8,12 @@
 import SwiftUI
 
 struct BarcodeScannerView: View {
+    @State private var scannedCode = ""
+    
     var body: some View {
         NavigationView {
             VStack {
-                ScannerView()
+                ScannerView(scannedCode: $scannedCode)
                     .frame(maxWidth: .infinity, maxHeight: 300)
                 
                 Spacer()
@@ -20,10 +22,10 @@ struct BarcodeScannerView: View {
                 Label("Scanned Barcode: ", systemImage: "barcode.viewfinder")
                     .font(.title)
                 
-                Text("Not Yet Scanned")
+                Text(scannedCode.isEmpty ? "Not Yet Scanned" : scannedCode)
                     .bold()
                     .font(.largeTitle)
-                    .foregroundColor(.green)
+                    .foregroundColor(scannedCode.isEmpty ? .red : .green)
                     .padding()
             }
             .navigationTitle("Barcode Scanner")
@@ -31,6 +33,8 @@ struct BarcodeScannerView: View {
     }
 }
 
-#Preview {
-    BarcodeScannerView()
+struct BarcodeScannerView_Previews: PreviewProvider {
+    static var previews: some View {
+        BarcodeScannerView()
+    }
 }
