5570a5ba8c695dc71312505a5a48885783b8fdc7
Gabriel Henrique <gabhenrique0@gmail.com>
2024-01-09 16:27:41 -0300

    Feature: add an alert item as a bindable state element on barcode scanner view

--- BarcodeScanner/BarcodeScannerView.swift
+++ BarcodeScanner/BarcodeScannerView.swift
@@ -7,13 +7,31 @@
 
 import SwiftUI
 
+struct AlertItem: Identifiable {
+    let id = UUID()
+    let title: String
+    let message: String
+    let dismissButton: Alert.Button
+}
+
+struct AlertContext {
+    static let invalidDeviceInput = AlertItem(title: "Invalid Device Input",
+                                              message: "Something is wrong with camera. We are unable to capture the input.",
+                                              dismissButton: .default(Text("Ok")))
+    
+    static let invalidScannedType = AlertItem(title: "Invalid Scan Type",
+                                              message: "The vlaue scanned is not valid. This app scans EAN-8 and EAN-13.",
+                                              dismissButton: .default(Text("Ok")))
+}
+
 struct BarcodeScannerView: View {
     @State private var scannedCode = ""
+    @State private var alertItem: AlertItem?
     
     var body: some View {
         NavigationView {
             VStack {
-                ScannerView(scannedCode: $scannedCode)
+                ScannerView(scannedCode: $scannedCode, alertItem: $alertItem)
                     .frame(maxWidth: .infinity, maxHeight: 300)
                 
                 Spacer()
@@ -29,6 +47,11 @@ struct BarcodeScannerView: View {
                     .padding()
             }
             .navigationTitle("Barcode Scanner")
+            .alert(item: $alertItem) { alertItem in
+                Alert(title: Text(alertItem.title),
+                      message: Text(alertItem.message),
+                      dismissButton: alertItem.dismissButton)
+            }
         }
     }
 }
